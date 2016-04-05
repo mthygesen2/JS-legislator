23dd28ee997a1e20f7a8f016bed54a2d406269c4
David Remington and Mao Thygesen <maothygesen@gmail.com>
2016-04-05 14:01:36 -0700

    add subcommittees from API, use committee API for house page

--- app/routes/house.js
+++ app/routes/house.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-   var url = 'http://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=false&apikey=fa966f56cda04703a3ab712e3d2a46ec';
-   return Ember.$.getJSON(url).then(function(responseJSON) {
-     return responseJSON.results;
-   });
- },
- showList: false,
- actions: {
-   subcommsShow() {
-     this.set('showList', true);
-   }
- }
+  model: function(params) {
+    var urlSubCom = 'https://congress.api.sunlightfoundation.com/committees?chamber=house&subcommittee=true&parent_committee_id=' + params.committee_id + '&apikey=fa966f56cda04703a3ab712e3d2a46ec';
+    var urlHouseCom = 'https://congress.api.sunlightfoundation.com/committees?committee_id=' + params.committee_id + '&chamber=house&subcommittee=false&apikey=fa966f56cda04703a3ab712e3d2a46ec';
+
+    return Ember.RSVP.hash({
+      subcom: Ember.$.getJSON(urlSubCom).then(function(responseJSON) {
+        return responseJSON.results;
+      }),
+      house: Ember.$.getJSON(urlHouseCom).then(function(responseJSON) {
+        return responseJSON.results;
+      }),
+    });
+  },
 });
